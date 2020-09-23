778a8867afa5c8d8ff34806e990fc0c42b1922d0
Erica Leonard <elynn1979@live.com>
2020-09-22 22:01:35 -0400

    halfway there

--- index.js
+++ index.js
@@ -59,12 +59,13 @@ Your function should add the flavor to the front of the array and console.log th
 
 For example addFlavor("Rainbow Sherbert", originalFlavors) should return ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla Burnt Almond"] */ 
 
-function addFlavor(/*code here*/){
+function addFlavor(array, newFlav){
+    array.unshift(newFlav)
 
     /*code here*/
-
+    console.log(array)
 }
-
+console.log(addFlavor(originalFlavors, "Rainbow Sherbert"))
 
 /* Task 3: Houston, we have a problem! There are now 32 flavors in the array! Your task is to remove an item from the end of the array. 
 
@@ -76,11 +77,14 @@ Your function should remove a flavor from the end of the array and console.log t
 
 For example removeLastFlavor(originalFlavors) would return ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla"]*/ 
 
-function removeLastFlavor(/*code here*/){
+function removeLastFlavor(array){
 
     /*code here*/
+    array.pop();
+    console.log(array)
 
 }
+console.log(removeLastFlavor(originalFlavors))
 
 /* Task 4: Write a function that returns a flavor at a given index in the array.
 
@@ -91,11 +95,12 @@ Your function should accept:
 
 For example, getFlavorByIndex(originalFlavors, 2) would return "Black Walnut", assuming Rainbow Sherbert has been added successfully. */
 
-function getFlavorByIndex(/*code here*/){
+function getFlavorByIndex(array,indexNum){
 
     /*code here*/
-
+    return array[indexNum]
 }
+console.log(getFlavorByIndex(originalFlavors, 17))
 
 /* Task 5: As corporate wants to add more and more flavors to their lineup, they've realized that they need to remove flavors based on flavor name, as opposed to just arbitrarily removing the first or last flavor. Your task is to get an index by flavor name, and remove that flavor from the array. 
 
@@ -110,9 +115,9 @@ Hint: You can use .splice() for this
 
 */
 
-function removeFlavorByName(/*code here*/){
-
-    /*code here*/
+function removeFlavorByName(array, flavor){
+    
+    
 
 }
 
